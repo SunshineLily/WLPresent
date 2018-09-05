538e0f36b8805bfa96c2c326e5d1b8b9a7476e3e
xiaoliwang <1587272615@qq.com>
2018-09-05 15:18:07 +0800

    commit

---
+++ WLPresent/Classes/WLPresent.h
@@ -0,0 +1,14 @@
+//
+//  WLPresent.h
+//  QKThWeeks
+//
+//  Created by wangxiaoli on 2018/9/5.
+//  Copyright © 2018年 王晓莉. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WLPresent : NSObject
+
+- (void)present;
+@end
---
+++ WLPresent/Classes/WLPresent.m
@@ -0,0 +1,16 @@
+//
+//  WLPresent.m
+//  QKThWeeks
+//
+//  Created by wangxiaoli on 2018/9/5.
+//  Copyright © 2018年 王晓莉. All rights reserved.
+//
+
+#import "WLPresent.h"
+
+@implementation WLPresent
+- (void)present
+{
+    NSLog(@"test");
+}
+@end
